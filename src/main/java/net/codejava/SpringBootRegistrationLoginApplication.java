b015f5f87137f75fe3a2da9df9e5ed2fac2f682a
Shinnbeegpp <marfajonard17@gmail.com>
2025-04-13 21:50:35 +0800

    dasd

--- src/main/java/net/codejava/SpringBootRegistrationLoginApplication.java
+++ src/main/java/net/codejava/SpringBootRegistrationLoginApplication.java
@@ -11,4 +11,3 @@ public class SpringBootRegistrationLoginApplication {
 	}
 
 }
-dasdas
--- src/main/java/net/codejava/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.codejava;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-	public User findByEmail(String email);	
-}
